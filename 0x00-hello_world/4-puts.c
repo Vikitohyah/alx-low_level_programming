013289a4173cf66d2478abff923fa67ecdc127aa
Victoria <victoriaedorh@gmail.com>
2022-12-08 13:46:28 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * main - Entry
+ * main - Entry point
+ *
  * Return: Always 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
