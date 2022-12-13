192a18108909f5f35a5a2d88242962e2fc9eb4fa
Victoria <victoriaedorh@gmail.com>
2022-12-13 14:16:21 -0800

    signs of alphabets

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,29 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * 0 if number is zero
- * -1 if number is less than zero
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ * @n : number to check
+ * Return: 0 0r 1
  */
 int print_sign(int n)
 {
+	int test;
+
 	if (n > 0)
 	{
+		test = 1;
 		_putchar('+');
-		return (1);
 	}
 	else if (n == 0)
 	{
+		test = 0;
 		_putchar('0');
-		return (0);
 	}
 	else if (n < 0)
 	{
+		test = -1;
 		_putchar('-');
-		return (-1);
 	}
+	return (test);
 }
