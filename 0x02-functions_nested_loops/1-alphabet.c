8d15baf9ca5f0810d5f4fc1193710a16a07d6756
Victoria <victoriaedorh@gmail.com>
2022-12-13 08:56:12 -0800

    .h lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(a);
+		alph++;
+	}
+	_putchar('\n');
+	return (0);
+}
