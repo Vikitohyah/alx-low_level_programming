4be82030545fd7cd11ea30e423b6fb69a82811ca
Victoria <victoriaedorh@gmail.com>
2022-12-15 06:14:40 -0800

    checks uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: a character parameter
- * Return: 0
+ * @c: the character
+ * Return: 1 if letter is uppercase, 0 if not
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (C >= 'A' && C <= 'Z')
 		return (1);
 	else
 		return (0);
