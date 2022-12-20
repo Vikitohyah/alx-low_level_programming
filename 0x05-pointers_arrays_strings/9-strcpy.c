daf3a2bcfb8b4b89df9e46b79b240ede4223f075
Victoria <victoriaedorh@gmail.com>
2022-12-19 16:51:48 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,12 @@ char *_strcpy(char *dest, char *src)
 {
 	char *start = dest;
 
-	while (*src != '⧵0')
+	while (*src != '\0')
 	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
-	*dest = '⧵0';
+	*dest = '\0';
 	return (start);
 }
