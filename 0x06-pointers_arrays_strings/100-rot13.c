89f212f84e1e31221a7c72ec72135c6c0e309a40
Victoria <victoriaedorh@gmail.com>
2022-12-23 01:06:20 -0800

    ROT13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * rot13 - encoder 13
+ * rot13 - encodes using rot13
  * @s:pointer to string parameters
- * Return: *s
+ * Return: encoded strings
  */
 char *rot13(char *s)
 {
@@ -19,7 +19,7 @@ char *rot13(char *s)
 		'k', 'l', 'm', 'n', 'o', 'p',
 		'q', 'r', 's', 't', 'u', 'v',
 		'w', 'x', 'y', 'z'};
-
+	
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 		'T', 'U', 'V', 'W', 'X', 'Y',
 		'Z', 'A', 'B', 'C', 'D', 'E',
@@ -29,7 +29,7 @@ char *rot13(char *s)
 		'x', 'y', 'z', 'a', 'b', 'c',
 		'd', 'e', 'f', 'g', 'h', 'i',
 		'j', 'k', 'l', 'm'};
-
+	
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		for (b = 0; b < 52; b++)
