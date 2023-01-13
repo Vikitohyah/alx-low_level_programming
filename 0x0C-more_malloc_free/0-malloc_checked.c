a8def63551d17676901908934b675e1579755399
Victoria <victoriaedorh@gmail.com>
2023-01-12 19:13:46 -0800

    nconcat

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  * Return: a pointer to the allocated memory
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
@@ -13,5 +15,6 @@ void *malloc_checked(unsigned int b)
 
 	if (ptr == NULL)
 		exit(98);
+
 	return (ptr);
 }
