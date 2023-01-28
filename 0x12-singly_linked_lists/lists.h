07aec3ff9236712180feaba1347b4e94d92435cf
Victoria <victoriaedorh@gmail.com>
2023-01-27 19:56:29 -0800

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
-#ifndef LIST_H
+#ifndef LISTS_H
 #define LISTS_H
 
+
+/**
+ * struct list_s - singly linked lists
+ * @str:  (malloc'ed string)
+ * @len: length of string
+ * @next: points to the next node
+ * Description: singly linked list mode for Holberton projects
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
